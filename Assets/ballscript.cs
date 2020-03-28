7b84dda075265f9ad8ab78e806113eaac89c17d9
AdityaWibisono17 <18523217@students.uii.ac.id>
2020-03-28 09:49:19 +0700

    update timer

--- Assets/ballscript.cs
+++ Assets/ballscript.cs
@@ -6,9 +6,12 @@ public class ballscript : MonoBehaviour
 {
     public int speed = 30;
     // Start is called before the first frame update
+    public Rigidbody2D sesuatu;
+
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(1,-1)* speed;
+        sesuatu.velocity = new Vector2(-1,-1)* speed;
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(1,-1)* speed;
     }
 
     // Update is called once per frame
